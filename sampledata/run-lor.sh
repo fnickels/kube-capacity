fc7ff7752d36747e03f16de983b459e1569261a5
Francis Nickels <fnickels@riotgames.com>
2023-05-03 13:48:48 -0700

    updates

---
+++ sampledata/run-lor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## QUICK
+
+just dolittle rc3-lor-dev usw2 dev
+
+just dolittle rc3-lor-lt usw2 lt
+
+just dolittle rc3-lor-prod apse1 asia
+just dolittle rc3-lor-prod euc1 europe
+just dolittle rc3-lor-prod usw2 americas
+just dolittle rc3-lor-prod usw2 playtest
+just dolittle rc3-lor-prod usw2 preprod
+
+## FULL
+
+just do rc3-lor-dev usw2 dev
+
+just do rc3-lor-lt usw2 lt
+
+just do rc3-lor-prod apse1 asia
+just do rc3-lor-prod euc1 europe
+just do rc3-lor-prod usw2 americas
+just do rc3-lor-prod usw2 playtest
+just do rc3-lor-prod usw2 preprod
+
---
+++ sampledata/run-val.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+## QUICK
+
+just dolittle rc3-val usw2 qa
+just dolittle rc3-val usw2 dev
+just dolittle rc3-val usw1 dev
+just dolittle rc3-val use1 dev
+just dolittle rc3-val euc1 dev
+just dolittle rc3-val atl1 dev
+just dolittle rc3-val apse2 dev
+just dolittle rc3-val apse1 dev
+
+just dolittle rc3-val-lt usw2 lt
+
+just dolittle rc3-val-prod usw2 prod
+just dolittle rc3-val-prod usw1 prod
+just dolittle rc3-val-prod use1 prod
+just dolittle rc3-val-prod sae1 prod
+just dolittle rc3-val-prod mia1 prod
+just dolittle rc3-val-prod mes1 prod
+just dolittle rc3-val-prod euw3 prod
+just dolittle rc3-val-prod euw2 prod
+just dolittle rc3-val-prod eun1 prod
+just dolittle rc3-val-prod euc1 prod
+just dolittle rc3-val-prod chi1 prod
+just dolittle rc3-val-prod atl1 prod
+just dolittle rc3-val-prod apse2 prod
+just dolittle rc3-val-prod apse1 prod
+just dolittle rc3-val-prod aps1 prod
+just dolittle rc3-val-prod apne2 prod
+just dolittle rc3-val-prod apne1 prod
+just dolittle rc3-val-prod ape1 prod
+
+## FULL
+
+just do rc3-val usw2 qa
+just do rc3-val usw2 dev
+just do rc3-val usw1 dev
+just do rc3-val use1 dev
+just do rc3-val euc1 dev
+just do rc3-val atl1 dev
+just do rc3-val apse2 dev
+just do rc3-val apse1 dev
+
+just do rc3-val-lt usw2 lt
+
+just do rc3-val-prod usw2 prod
+just do rc3-val-prod usw1 prod
+just do rc3-val-prod use1 prod
+just do rc3-val-prod sae1 prod
+just do rc3-val-prod mia1 prod
+just do rc3-val-prod mes1 prod
+just do rc3-val-prod euw3 prod
+just do rc3-val-prod euw2 prod
+just do rc3-val-prod eun1 prod
+just do rc3-val-prod euc1 prod
+just do rc3-val-prod chi1 prod
+just do rc3-val-prod atl1 prod
+just do rc3-val-prod apse2 prod
+just do rc3-val-prod apse1 prod
+just do rc3-val-prod aps1 prod
+just do rc3-val-prod apne2 prod
+just do rc3-val-prod apne1 prod
+just do rc3-val-prod ape1 prod
+
+
